2dcd93ee4da44fa223f9aca1adf2e78b61cd66dd
Christoph Heindl <cheind@profactor.at>
2022-12-05 13:38:47 +0100

    swapped definition for recurrent one

--- torchsh/symbolic/rsh.py
+++ torchsh/symbolic/rsh.py
@@ -10,17 +10,19 @@ We use the Herglotzian definition to generate three polynomials
 associated with x,y and z coordinates. See "Separated Cartesian form" [1]
 for details.
 
-
 The reason we don't use `sympy.Znm`, which takes spherical coordinates,
 is that I did not manage to simplify/substitute the resulting expressions
 enough so that they become simple functions of x,y,z respectively.
+
+In the current implementation we use code adapted from
+https://github.com/NVlabs/tiny-cuda-nn/blob/8e6e242f36dd197134c9b9275a8e5108a8e3af78/scripts/gen_sh.py
+which uses a recurrent definition. It turned out to be more stable during tests.
 
 References:
     - https://en.wikipedia.org/wiki/Spherical_harmonics#Separated_Cartesian_form
     - http://en.citizendium.org/wiki/Solid_harmonics#In_total
     - https://www.osti.gov/pages/servlets/purl/1172304
 """
-
 
 try:
     import sympy as sym
@@ -29,47 +31,47 @@ except ImportError:
         "Failed to import sympy. You need `pip install sympy` for this part of the"
         " code."
     )
-import math
 
 
-theta, phi = sym.symbols("phi, theta", real=True)
-x, y, z = sym.symbols("x:z", real=True)
+def _init_sin_cos_terms(n: int, x: sym.Symbol, y: sym.Symbol, z: sym.Symbol):
+    S = [0]
+    C = [1]
+    for i in range(10):
+        S.append(sym.simplify(x * S[i] + y * C[i]))
+        C.append(sym.simplify(x * C[i] - y * S[i]))
+    return S, C
 
 
-def A(m: int, x: sym.Symbol, y: sym.Symbol) -> sym.Expr:
-    """A polynom associated with x,y"""
-    p = sym.symbols("p", integer=True)
-    f = sym.binomial(m, p) * x**p * y ** (m - p) * sym.cos((m - p) * sym.pi / 2)
-    return sym.Sum(f, (p, 0, m))  # (sum is inclusive in sympy)
-
-
-def B(m: int, x: sym.Symbol, y: sym.Symbol) -> sym.Expr:
-    """B polynom associated with x,y"""
-    p = sym.symbols("p", integer=True)
-    f = sym.binomial(m, p) * x**p * y ** (m - p) * sym.sin((m - p) * sym.pi / 2)
-    return sym.Sum(f, (p, 0, m))  # (sum is inclusive in sympy)
-
-
-def P(n: int, m: int, z: sym.Symbol) -> sym.Expr:
-    """Polynom associated with z"""
-    k = sym.symbols("k", integer=True)
-    scale = sym.sqrt(sym.factorial(n - m) / sym.factorial(n + m))
-    phase = (-1) ** k
-    gamma = phase * 2 ** (-n) * sym.binomial(n, k) * sym.binomial(2 * n - 2 * k, n)
-    gamma = gamma * sym.factorial((n - 2 * k)) / sym.factorial(n - 2 * k - m)
-    f = gamma * z ** (n - 2 * k - m)
-    upper = int(math.floor((n - m) / 2))
-    return scale * sym.Sum(f, (k, 0, upper))
-
-
-def Ynm(n: int, m: int, x: sym.Symbol, y: sym.Symbol, z: sym.Symbol) -> sym.Expr:
+def Ynm(n: int, m: int, x: sym.Symbol, y: sym.Symbol, z: sym.Symbol):
     """Return a symbolic expression for the real spherical
-    harmonics having degree `l` and order `m`."""
-    assert abs(m) <= n
-    if m < 0:
-        f = sym.sqrt((2 * n + 1) / (2 * sym.pi)) * P(n, abs(m), z) * B(abs(m), x, y)
-    elif m == 0:
-        f = sym.sqrt((2 * n + 1) / (4 * sym.pi)) * P(n, 0, z)
-    else:
-        f = sym.sqrt((2 * n + 1) / (2 * sym.pi)) * P(n, m, z) * A(m, x, y)
-    return f.doit().simplify()
+    harmonics having degree `l` and order `m`.
+
+    Adapted from
+    https://github.com/NVlabs/tiny-cuda-nn/blob/8e6e242f36dd197134c9b9275a8e5108a8e3af78/scripts/gen_sh.py
+    """
+    S, C = _init_sin_cos_terms(n, x, y, z)
+
+    def K(n, m):
+        return sym.sqrt(
+            (2 * n + 1)
+            * sym.factorial(n - abs(m))
+            / (4 * sym.pi * sym.factorial(n + abs(m)))
+        )
+
+    def P(n, m):
+        if n == 0 and m == 0:
+            return 1
+        if n == m:
+            return (1 - 2 * m) * P(m - 1, m - 1)
+        if n == m + 1:
+            return (2 * m + 1) * z * P(m, m)
+        return ((2 * n - 1) * z * P(n - 1, m) - (n + m - 1) * P(n - 2, m)) / (n - m)
+
+    def Y(n, m):
+        if m > 0:
+            return sym.sqrt(2) * K(n, m) * C[m] * P(n, m)
+        if m < 0:
+            return sym.sqrt(2) * K(n, m) * S[-m] * P(n, -m)
+        return K(n, m) * P(n, m)
+
+    return Y(n, m)
